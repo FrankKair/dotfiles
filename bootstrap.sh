0675f06cc4572a34c29cf6c8aa2a3aa8ef91fcf2
Frank Kair <frankkair@gmail.com>
2020-02-13 00:07:10 +0000

    typescript from npm to brew

--- bootstrap.sh
+++ bootstrap.sh
@@ -29,27 +29,25 @@ brew cask install google-backup-and-sync
 
 echo "~ Tools"
 brew install neovim
-brew install yarn
-brew install fzf
 brew install bat
 brew install diff-so-fancy
-brew install rbenv
+brew install fzf
+brew install yarn
 
 echo "~ vim-plug"
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 echo "~ Programming Languages"
-brew install node
+brew install typescript
+brew install go
 brew install python3
 brew install lua
-brew install go
 brew install ocaml
 brew install opam
 
-echo "~ TypeScript & ts-node"
+echo "~ ts-node"
 npm install -g ts-node
-npm install -g typescript
 
 echo "~ rustup"
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
