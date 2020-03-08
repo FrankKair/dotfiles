e2ef84df155e32bdd91abb024e32576a94a458eb
Frank Kair <frankkair@gmail.com>
2020-03-08 05:54:47 +0000

    Adds Emacs config

---
+++ .emacs.d/init.el
@@ -0,0 +1,35 @@
+;; Set up package.el to work with MELPA
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/"))
+(package-initialize)
+(package-refresh-contents)
+
+;; Download Evil
+(unless (package-installed-p 'evil)
+  (package-install 'evil))
+
+;; Enable Evil
+(require 'evil)
+(evil-mode 1)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (evil))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+;; Theme
+(add-to-list 'load-path "~/.emacs.d/pastel-tron-theme/")
+(add-to-list 'custom-theme-load-path "~/.emacs.d/pastel-tron-theme/")
+(require 'pastel-tron-theme)
+(load-theme `pastel-tron t)
+
+;; Turns off alarm/bell sound 
+(setq ring-bell-function 'ignore)
