bd8e6a8b0cbfb03ab9fe50d0f6775016ea25ad53
Frank Kair <frankkair@gmail.com>
2021-05-03 14:24:43 +0100

    Removes workspace switching animations (+ others)

--- macos.sh
+++ macos.sh
@@ -1,3 +1,18 @@
 # Faster key repeats
 defaults write -g InitialKeyRepeat -int 10
 defaults write -g KeyRepeat -int 1
+
+# Remove workspace animations
+defaults write com.apple.dock workspaces-swoosh-animation-off -bool YES
+defaults write com.apple.dock expose-animation-duration -float 0.20; killall Dock
+
+# Faster launchpad
+defaults write com.apple.dock springboard-show-duration -float 0.10
+defaults write com.apple.dock springboard-hide-duration -float 0.10
+
+# No .DS_Store files
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
+
+# defaults write -g AppleDisplayScaleFactor -float
+# defaults write -g NSNavPanelIconViewIconSizeForOpenMode -number
