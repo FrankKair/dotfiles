777a9e783fceded3842c473326528f2d79e201b4
Frank Kair <frankkair@gmail.com>
2021-07-08 10:32:53 +0100

    Adds mouse tracking speed to macos.sh

--- macos.sh
+++ macos.sh
@@ -2,13 +2,9 @@
 defaults write -g InitialKeyRepeat -int 10
 defaults write -g KeyRepeat -int 1
 
-# Remove workspace animations
-defaults write com.apple.dock workspaces-swoosh-animation-off -bool YES
-defaults write com.apple.dock expose-animation-duration -float 0.20; killall Dock
+# Mouse tracking speed
+defaults write -g com.apple.mouse.scaling  14.0
 
 # No .DS_Store files
 defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
 defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
-
-# defaults write -g AppleDisplayScaleFactor -float
-# defaults write -g NSNavPanelIconViewIconSizeForOpenMode -number
