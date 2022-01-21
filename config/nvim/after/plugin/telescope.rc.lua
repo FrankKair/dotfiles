9a94270571d90ef0d4a10a4ebe110258a9276212
Frank Kair <frankkair@gmail.com>
2022-01-21 10:53:27 +0000

    Adds git_status & file_browser to telescope.rc.lua

--- config/nvim/after/plugin/telescope.rc.lua
+++ config/nvim/after/plugin/telescope.rc.lua
@@ -1,7 +1,9 @@
 local map = vim.api.nvim_set_keymap
 local opts = { noremap = true }
 
-map('n', '<Leader>p', '<cmd>Telescope find_files<CR>', opts)
-map('n', '<Leader>;', '<cmd>Telescope buffers<CR>', opts)
+map('n', '<Leader>f', '<cmd>Telescope find_files<CR>', opts)
+map('n', '<Leader>b', '<cmd>Telescope buffers<CR>', opts)
 map('n', '<Leader>g', '<cmd>Telescope live_grep<CR>', opts)
 map('n', '<Leader>h', '<cmd>Telescope help_tags<CR>', opts)
+map('n', '<Leader>gs', '<cmd>Telescope git_status<CR>', opts)
+map('n', '<Leader>t', '<cmd>Telescope file_browser<CR>', opts)
