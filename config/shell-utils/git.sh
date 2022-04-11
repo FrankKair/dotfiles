858c1977a21d42c9533e71b97314a45694e78b8f
Frank Kair <frankkair@gmail.com>
2022-04-11 15:32:15 +0100

    Uses fzf for gbc git alias and adds gshow function
    gshow uses fzf to search commits and then uses git show

--- config/shell-utils/git.sh
+++ config/shell-utils/git.sh
@@ -1,6 +1,15 @@
 alias gcane='git commit --amend --no-edit && git push --force'
-alias gbc='git branch | cat'
+alias gbc='git checkout $(git branch | fzf)'
 alias gprune="git branch --merged master | grep -v '^[ *]*master$' | xargs git branch -d"
+
+gshow () {
+  if [ $# -eq 0 ]; then
+    COMMIT_HASH=$(git log --oneline | fzf --multi --preview 'git show {+1}' | awk '{print $1}')
+    eval "git show $COMMIT_HASH"
+  else
+    eval "git show $1"
+  fi
+}
 
 setupstreambranch () {
   # BRANCH=`git branch | cat | grep -i '*' | cut -c 3-`
