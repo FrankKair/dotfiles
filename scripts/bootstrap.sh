d1999d61d8eb8c98928e731b27646e3d39997d61
Frank Kair <frankkair@gmail.com>
2023-08-11 11:57:13 +0100

    Removes and/or comments out apps from bootstrap

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -21,24 +21,18 @@ brew install --cask vlc
 brew install --cask calibre
 brew install --cask anki
 brew install --cask basictex
-
 brew install --cask rectangle
 brew install --cask maccy
-brew install --cask clipy
-brew install --cask hiddenbar
 brew install --cask mouse-fix
 brew install --cask fontforge
-
 brew install --cask firefox
 brew install --cask finicky
-
 brew install --cask google-drive
 brew install --cask spotify
 brew install --cask slack
 
 echo "~ Developer tools"
 brew install --cask alacritty
-brew install --cask iterm2
 brew install --cask visual-studio-code
 brew install --cask emacs
 brew install --cask insomnia 
@@ -47,36 +41,33 @@ brew install --cask docker
 
 echo "~ CLIs"
 brew install neovim
-brew install tig
 brew install gitui
 brew install diff-so-fancy
 brew install gh
-
 brew install fzf
 brew install ripgrep
 brew install bat
 brew install exa
-
 brew install pandoc
 brew install translate-shell
 brew install tmux
-
 brew install shellcheck
 brew install infer
-
-brew install koekeishiya/formulae/skhd
 
 echo "~ vim-plug"
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-#brew install screenfetch
-#brew install --cask google-chrome
-#brew install --cask brave-browser
-#brew install --cask openinterminal
-#brew install --cask amethyst
-#brew install --cask lunar
-#brew install --cask fsnotes
-#brew install --cask background-music
-#brew install --cask eul
-#brew install --cask alt-tab
+# brew install screenfetch
+# brew install koekeishiya/formulae/skhd
+# brew install --cask iterm2
+# brew install --cask hiddenbar
+# brew install --cask google-chrome
+# brew install --cask brave-browser
+# brew install --cask openinterminal
+# brew install --cask amethyst
+# brew install --cask lunar
+# brew install --cask fsnotes
+# brew install --cask background-music
+# brew install --cask eul
+# brew install --cask alt-tab
