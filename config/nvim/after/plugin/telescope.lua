349b68e315a9238ad0ab7ab9ca457fc5f9fa2a9b
Frank Kair <frankkair@gmail.com>
2023-08-29 16:58:12 +0100

    Telescope find & grep includes hidden files

--- config/nvim/after/plugin/telescope.lua
+++ config/nvim/after/plugin/telescope.lua
@@ -12,6 +12,16 @@ require('telescope').setup {
       },
     },
   },
+  pickers = {
+    find_files = {
+      hidden = true,
+    },
+    live_grep = {
+      additional_args = function()
+        return { '--hidden' }
+      end
+    },
+  },
   extensions = {
     file_browser = {
       theme = 'dropdown',
