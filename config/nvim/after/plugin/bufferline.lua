3fd4c5418c6a2bd19004bc15b9a06c30ea2b0a36
Frank Kair <frankkair@gmail.com>
2025-05-22 09:16:00 +0100

    Removes nvim from dotfiles (created new repo)

--- config/nvim/after/plugin/bufferline.lua
+++
@@ -1,6 +0,0 @@
-require('bufferline').setup {}
-
-local opts = { noremap = true, silent = true }
-
-vim.keymap.set('n', '}', ':BufferLineCycleNext<CR>', opts)
-vim.keymap.set('n', '{', ':BufferLineCyclePrev<CR>', opts)
