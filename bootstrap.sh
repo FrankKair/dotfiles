bf3e6d51fad9ef0d8360bdb498029924416e369d
Frank Kair <frankkair@gmail.com>
2026-05-17 11:51:59 +0100

    Adds Iosevka font

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env sh
 xcode-select --install
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-brew install font-hack-nerd-font
 brew install --cask the-unarchiver
 brew install --cask rectangle
 brew install --cask maccy
@@ -11,6 +10,7 @@ brew install --cask spotify
 brew install --cask transmission
 brew install --cask vlc
 brew install --cask ghostty
+brew install font-iosevka
 brew install neovim
 brew install fzf
 brew install ripgrep
